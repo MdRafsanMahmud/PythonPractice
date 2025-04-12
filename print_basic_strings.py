402c2cec625a3dee624aad2b4dae03b2bd95c6e4
MdRafsanMahmud <mdrafsanmahmud99@gmail.com>
2025-04-12 09:29:54 +0600

    Initial commit

---
+++ print_basic_strings.py
@@ -0,0 +1,29 @@
+# 1. Print a short story using multiple lines.
+# TODO:
+print("\nTODO-1:")
+print("Once upon a time in a quiet village,")
+print("there lived a boy named Rafsan who loved solving puzzles.")
+print()
+print("Every evening, he sat under a big banyan tree,\nwondering how the stars managed to shine without fuel.")
+
+print('''\nOne day, he discovered Python — not the snake, but the language
+And from that day on, he started coding his own little magic 
+The village was never the same again.''')
+
+# 2. Create a string that includes both single and double quotes.
+# TODO:
+print("\nTODO-2:")
+quote = 'He said, "Python is amazing!" and continued writing.'
+print(quote)
+
+# 3. Use escape characters to create a formatted list.
+# TODO:
+print("\nTODO-3:")
+print("List:\n\tA = Alpha\n\tB = Beta\n\tC = Charlie\n\tD = Delta")
+
+# 4. Combine different types of data (strings and numbers) in one print statement.
+# TODO:
+print("\nTODO-4:")
+age = 25
+name = "Rafsan"
+print(f"My name is {name}, and I am {age} years old.")
