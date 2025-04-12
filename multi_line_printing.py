2bda87954d292d00dba4e21070eaef5747292f04
MdRafsanMahmud <mdrafsanmahmud99@gmail.com>
2025-04-12 09:02:44 +0600

    Initial commit

---
+++ multi_line_printing.py
@@ -0,0 +1,26 @@
+# Try printing a simple three-line text or poem using three separate print() statements.
+#TODO:
+print("\nTODO-1:")
+print("Hi I am Md Rafsan Mahmud")
+print("I am a student of Computer Science")
+print("I recently graduated from East West University, Dhaka")
+# Try printing a simple three-line text or poem using one print() statement with \n.
+#TODO:
+print("\nTODO-2:")
+print("Hi I am Md Rafsan Mahmud\nI am a student of Computer Science\nI recently graduated from East West University, Dhaka")
+
+# Print it using triple quotes.
+#TODO:
+print("\nTODO-3:")
+print("""Hi I am Md Rafsan Mahmud
+I am a student of Computer Science
+""")
+
+# Add a blanc line between each sting/verse of your text/poem using any method you prefer.
+#TODO:
+print("\nTODO-4:")
+print("""Hi I am Md Rafsan Mahmud
+
+I am a student of Computer Science
+
+""")
