3b143613af0b1647d8d0ca00a4f4b1fa3f253879
MdRafsanMahmud <mdrafsanmahmud99@gmail.com>
2025-04-12 09:52:53 +0600

    Initial commit

---
+++ common_mistakes_of_print.py
@@ -0,0 +1,19 @@
+# 1. print('Let's practice finding errors!')
+# 2.  print('Let's practice finding errors!')
+# 3. print'What's wrong with this line?'
+# 4. Print('Fix me')
+# 5. print('What is missing here?'
+# 6.      print("What do I use" +, + "incorrectly here?")
+
+#1 Using double quotes
+print("Let's practice finding errors!")
+#2 Removing indentation and using backslash for single quote
+print('Let\'s practice finding errors!')
+#3 Adding parenthesis and backslash for quote
+print('What\'s wrong with this line?')
+#4 Use lowercase for print syntax
+print('Fix me')
+#5 Closing parenthesis missing
+print('What is missing here?')
+#6 Removing indentation and add comma inside the double quoatation
+print("What do I use" + "," + "incorrectly here?")
