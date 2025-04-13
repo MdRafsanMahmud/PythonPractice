d82217d8810d1c421675b26019518c5b09bc5f52
MdRafsanMahmud <mdrafsanmahmud99@gmail.com>
2025-04-13 06:23:18 +0600

    Initial commit

---
+++ data_type_id.py
@@ -0,0 +1,19 @@
+# For each of the following values determine data types:
+'''42
+3.14
+"Hello, World!"
+True
+-7
+"42"
+False
+0.0
+'''
+
+print(type(42))
+print(type(3.14))
+print(type("Hello, World!"))
+print(type(True))
+print(type(-7))
+print(type("42"))
+print(type(False))
+print(type(0.0))
