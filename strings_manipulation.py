c557f0a38c90c230fae4a9627f5d0d1bdfd77b8c
MdRafsanMahmud <mdrafsanmahmud99@gmail.com>
2025-04-16 15:09:47 +0600

    Initial commit

---
+++ strings_manipulation.py
@@ -0,0 +1,20 @@
+# Create a variable first_name and assign to it your first name.
+first_name = "Rafsan"
+
+# Create a variable last_name and assign to it your last name.
+last_name = "Mahmud"
+
+# Create a variable full_name by concatenating first_name and last_name with a space in between.
+full_name = first_name + " " + last_name
+
+#  Print out the full_name.
+print(full_name)
+
+# Print out the length of full_name using the len()function.
+print("Length of fullname:",len(full_name))
+
+# Print out the full_name in all uppercase letters.
+print(full_name.upper())
+
+# Print out the full_name in all lowercase letters.
+print(full_name.lower())
