833e28b6a6cbd960eb6e5b2d5298335057f97c0b
MdRafsanMahmud <mdrafsanmahmud99@gmail.com>
2025-04-20 18:13:24 +0600

    Initial commit

--- user_input_exercises.py
+++ user_input_exercises.py
@@ -56,8 +56,53 @@ dogs_years = int(user_age) * 7
 print(f"Your are {user_age} old and Your age in dog years is: {dogs_years}")
 
 
+# 8. Simple Calculator
+# Create a program that prompts for two numbers and operation (+, -, *, /),
+# performs the calculation, and displays the result.
+
+first_value = float(input("Enter first value: "))
+second_value = float(input("Enter second value: "))
+addition = first_value + second_value
+subtraction = first_value - second_value
+multiplication = first_value * second_value
+division = first_value / second_value
+
+print(f"Addition: {addition}")
+print(f"Subtraction: {subtraction}")
+print(f"Multiplication: {multiplication}")
+print(f"Division: {division}")
+
+# 9. Days to Hours
+# Develop a program that asks for a number of days, converts it into an integer,
+# then calculates and prints the equivalent number in hours.
+#
+# num_of_days = int(input("Please enter the number of days: "))
+# print(f"{num_of_days} days is equivalent to {num_of_days * 24} hours:")
 
 
+# 10. Mad Libs
+# Create a simple Mad Libs game. Ask the user for an adjective, a noun, and a verb,
+# then use these inputs to compltete a pre-written sentence.
+
+adjective = input("Enter an adjective: ")
+noun = input("Enter a noun: ")
+verb = input("Enter a verb: ")
+
+print(f"The {adjective} {noun} decided to {verb} all day long.")
+
+
+
+# 11. Story Generator
+# Write a program that requests a character name, a place, and an object, then
+# weaves these elements into a short short story.
+
+character = input("Enter a character name: ")
+place = input("Enter a place: ")
+object_ = input("Enter an object: ")
+
+print(f"\nOnce upon a time in {place}, there lived a brave soul named {character}.")
+print(f"One day, {character} discovered a mysterious {object_} hidden deep in the forest.")
+print(f"With courage in their heart and the {object_} in hand, {character} set off on an unforgettable adventure.")
 
 
 
